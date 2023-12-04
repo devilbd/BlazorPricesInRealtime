3c982c2f6aa574fe8205085143cf4661e19787c1
Vladimir Parchev <20968791+devilbd@users.noreply.github.com>
2023-12-04 11:24:18 +0200

    ...

--- BlazorPricesInRealtime/Components/Shared/SearchList.razor.cs
+++ BlazorPricesInRealtime/Components/Shared/SearchList.razor.cs
@@ -5,17 +5,13 @@ namespace BlazorPricesInRealtime.Components.Shared
 {
     public partial class SearchList : ComponentBase
     {
-
-
+        private bool CoerceText;
+        private bool CoerceValue;
         protected override async Task OnInitializedAsync()
         {
             
         }
         public string SelectedSymbol { get; set; }
-
-        private bool ResetValueOnEmptyText;
-        private bool CoerceText;
-        private bool CoerceValue;
 
         private string[] Symbols =
         {
@@ -25,24 +21,13 @@ namespace BlazorPricesInRealtime.Components.Shared
         private async Task<IEnumerable<string>> OnSymbolSelected(string value)
         {
             // In real life use an asynchronous function for fetching data from an api.
-            //await Task.Delay(5);
+            await Task.Delay(5);
 
-            //// if text is null or empty, show complete list
-            //if (string.IsNullOrEmpty(value))
-            //    return Symbols;
-            //var symbolsListResult = Symbols.Where(x => x.Contains(value, StringComparison.InvariantCultureIgnoreCase));
-            //return Task.FromResult(symbolsListResult).Result;
-            return Symbols;
-        }
-
-        private void HandleOpenChanged(bool isOpen)
-        {
-            // MudBlazor will close the control after OnValueChanged is called
-            // So check if it's closed, and our B List trigger has been selected, and just open it again if it is
-            //if (!isOpen && bListSelected)
-            //{
-            //    ac.ToggleMenu();
-            //}
+            // if text is null or empty, show complete list
+            if (string.IsNullOrEmpty(value))
+                return Symbols;
+            var symbolsListResult = Symbols.Where(x => x.Contains(value, StringComparison.InvariantCultureIgnoreCase));
+            return symbolsListResult;
         }
     }
 }
